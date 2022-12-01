586dd3bd8b9c6cce774339444b2a2b2e618e1fba
Duarte Santos <76753573+Dzone07@users.noreply.github.com>
2022-12-01 16:55:33 +0000

    Update Lab1_P1.m

--- Lab1_P1.m
+++ Lab1_P1.m
@@ -1,39 +1,43 @@
-
-n_it = 150;
-h = 1;
-v1 = 3110;
-v2 = 3110;
-k10 = 0.0643 * 3600;
+clear
+%Constantes ja definidas
 k12 = 0.3 * 3600;
 k21 = 0.2455 * 3600;
+k10 = 0.0643 * 3600;
+v1 = 3110;
+v2 = 3110;
 delta = 1000;
+h = 1;
 
-dc1(1:n_it)=zeros;
-dc2(1:n_it)=zeros;
-c1(1:n_it)=zeros;
-c2(1:n_it)=zeros;
-d(1:(n_it/6))=3;
-d=upsample(d,6,0);
+%Valores das varias posiçoes da matriz da equaçao (1)
+mat_A = -1 * (k12 + k10)/v1;
+mat_B = k21/v1;
+mat_C = k12/v2;
+mat_D = -1 * k21/v2;
+A = [mat_A, mat_B; mat_C, mat_D];
+%valores selecionados
+xmax = 60;
+d_val = 3;
 
-t(1:n_it) = zeros;
+%definiçao dos vetores t, d, c1 e c2
+t = 0:1:xmax-1;
+d = 1:1:xmax/6;
+d(1:xmax/6) = d_val;
+d=upsample(d,6);
+c1(1:xmax-1) = zeros;
+c2(1:xmax-1) = zeros;
 
-A = [1/v1 * (-k12-k10),1/v1 * k21; 1/v2 *k12 , -1/v2 *k21];
-D = [1/v1;0] * delta;
-
-for k=1:n_it
-    change = A *[c1(k);c2(k)] + D*d(k);
-    c1(k+1)=c1(k)+h*change(1);
-    c2(k+1)=c2(k)+h*change(2);
-
-    t(k) = k * h;
+for k = 1:xmax-1
+    c1(h*(k+1)) = c1(h*k) + h * A(1,:) * [c1(h*k); c2(h*k)] + delta * d(h*k)/v1;
+    c2(h*(k+1)) = c2(h*k) + h * A(2,:) * [c1(h*k); c2(h*k)];
 end
 
-
+%Plot dos graficos, sobrepostos
 figure(1)
-plot(t,c1(1:n_it))
+plot(t,c1,'-r')
 hold on
-plot(t,c2(1:n_it))
-
-%gg=plot(t,c1,c2);
-%set(gg,'LineWidth',1.5)
-
+plot(t,c2,'-b')
+hold on
+plot(t,d,'-g')
+title("Evolução no tempo da dose e das concentrações de efeito");
+xlabel("tempo t (dias)");
+ylabel("c1 (mg/kg) a vermelho, c2 (mg/kg) a azul, d (mg) a verde");
